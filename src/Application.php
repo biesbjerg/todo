3dd2b90a76c4d4d6cbeff37d0552d272aac24226
Kim Biesbjerg <kb@mindly.dk>
2020-07-06 15:26:34 +0200

    add IdeHelper plugin

--- src/Application.php
+++ src/Application.php
@@ -115,7 +115,6 @@ class Application extends BaseApplication
         }
 
         $this->addPlugin('Migrations');
-
-        // Load more plugins here
+        $this->addPlugin('IdeHelper');
     }
 }

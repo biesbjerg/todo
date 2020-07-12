7f9cf0b8add4a3b62091f59d134825ce550a7f0d
Kim Biesbjerg <kb@mindly.dk>
2020-07-12 11:14:42 +0200

    Fix invalid redirect after deleting a list

--- src/Controller/TodoListsController.php
+++ src/Controller/TodoListsController.php
@@ -123,6 +123,6 @@ class TodoListsController extends AppController
             $this->Flash->error(__('The todo list could not be deleted. Please, try again.'));
         }
 
-        return $this->redirect($this->referer());
+        return $this->redirect(['_name' => 'view_smart_todo_list', 'my-day']);
     }
 }
